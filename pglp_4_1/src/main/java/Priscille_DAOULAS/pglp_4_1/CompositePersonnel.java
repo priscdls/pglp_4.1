40a6215013e7a5d99064f845f932ac8b02e5ab76
Priscille Daoulas <priscilledaoulas@gmail.com>
2020-03-07 21:01:12 +0100

    Checkstyle et javadoc de l'Interface et du Composite

--- pglp_4_1/src/main/java/Priscille_DAOULAS/pglp_4_1/CompositePersonnel.java
+++ pglp_4_1/src/main/java/Priscille_DAOULAS/pglp_4_1/CompositePersonnel.java
@@ -1,21 +1,56 @@
 package Priscille_DAOULAS.pglp_4_1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
+/**
+ * Pattern Composite CompositePersonnel implémentant l'interface InterfacePersonnel.
+ */
 public class CompositePersonnel implements InterfacePersonnel {
+	/**
+	 * Liste de membres du personnels d'un même composite.
+	 */
 	private ArrayList<InterfacePersonnel> list = new ArrayList<InterfacePersonnel>();
-	
+	/**
+	 * Identifiant d'un composite.
+	 */
+	private final int id;
+	/**
+	 * Identifiant du composite suivant.
+	 */
+	private static int idFin = 1;
+	/**
+	 * Constructeur.
+	 */
+	public CompositePersonnel() {
+		id = idFin++;
+		list = new ArrayList<InterfacePersonnel>();
+	}
+	/**
+	 * Fonction d'écriture des informations du composite.
+	 */
 	public void print() {
+		System.out.println("Id : "+id);
 		for(InterfacePersonnel intP : list) {
 			intP.print();
 		}
 	}
-	
-	public void add(InterfacePersonnel intP) {
+	/**
+	 * Fonction qui ajoute une personne a la liste du personnel.
+	 * @param intP La personne a ajouter
+	 * @return Le composite
+	 */
+	public CompositePersonnel add(final InterfacePersonnel intP) {
 		list.add(intP);
+		return this;
 	}
-	
-	public void remove(InterfacePersonnel intP) {
+	/**
+	 * Fonction qui enlève une personne a la liste du personnel.
+	 * @param intP La personne a enlever
+	 * @return Le composite
+	 */
+	public CompositePersonnel remove(final InterfacePersonnel intP) {
 		list.remove(intP);
+		return this;
 	}
 }
--- pglp_4_1/src/main/java/Priscille_DAOULAS/pglp_4_1/InterfacePersonnel.java
+++ pglp_4_1/src/main/java/Priscille_DAOULAS/pglp_4_1/InterfacePersonnel.java
@@ -1,6 +1,10 @@
 package Priscille_DAOULAS.pglp_4_1;
-
+/**
+ * Interface InterfacePersonnel utilisé pour le Pattern Composite.
+ */
 public interface InterfacePersonnel {
+	/**
+	 * Fonction d'affichage.
+	 */
 	public void print();
-
 }
